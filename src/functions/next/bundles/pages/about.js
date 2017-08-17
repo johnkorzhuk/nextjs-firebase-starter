a183938f74d61cf3f6dc125e869bcd467f0cc244
John Korzhuk <jkorzhuk@gmail.com>
2017-08-17 15:33:55 -0400

    init commit

---
+++ src/functions/next/bundles/pages/about.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/about', function() {
+            var comp = module.exports=webpackJsonp([5],{349:function(e,t,u){e.exports=u(350)},350:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(9),o=n(l),r=u(184),a=n(r);t.default=function(){return o.default.createElement(a.default,null,o.default.createElement("p",null,"About Page"))}}},[349]);
+            return { page: comp.default }
+          })
+        
